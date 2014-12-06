e74567d87296f8d4e6cebdbd96e73200dbd591cc
Darkwing1012 <dylan8522@gmail.com>
2014-12-06 15:17:47 -0500

    Update

--- extension.js
+++ extension.js
@@ -44,6 +44,18 @@
                 }
             }
         };
+		bot.commands.loveCommand = {
+            command: 'love',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me I Love You <3!!!");
+                }
+            }
+        };
 
         //Load the chat package again to account for any changes
         bot.loadChat();
@@ -84,7 +96,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 5,
-        motd: "What should i put here?",
+        motd: "",
         filterChat: true,
         etaRestriction: false,
         welcome: true,
