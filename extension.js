c27a669e5656881aa21e6b3a49d4a586f44809c3
Darkwing1012 <dylan8522@gmail.com>
2014-12-06 15:38:23 -0500

    update

--- extension.js
+++ extension.js
@@ -32,15 +32,16 @@
 
          */
 
-        bot.commands.plugCommand = {
-            command: 'plug',  //The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.Command = {
+            command: 'love',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("Download Tastyplug from https://fungustime.pw/tastyplug !");
+                    API.sendChat("Download Tastyplug from https://fungustime.pw/tastyplug/ !");
+					API.sendChat("/me Pls No");
                 }
             }
         };
