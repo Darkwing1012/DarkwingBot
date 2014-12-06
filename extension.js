32008535e4adfe7f3b60cd5462c682ea95f33f27
Darkwing1012 <dylan8522@gmail.com>
2014-12-06 16:32:10 -0500

    Update

--- extension.js
+++ extension.js
@@ -35,12 +35,12 @@
         bot.commands.tastyplugCommand = {
             command: 'tastyplug',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            type: 'exact', //Specify if it can accept variables or not (vif so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("Download Tastyplug from https://fungustime.pw/tastyplug/ !");
+                    API.sendChat("Download Tastyplug from https://fungustime.pw/tastyplug/ to see special emotes like :ravecat:");
                 }
             }
         };
