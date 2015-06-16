8e3d22448105694f90dec1a834a450a892b7ae89
Darkwing1012 <dylan8522@gmail.com>
2015-06-16 12:34:52 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -33,14 +33,14 @@
          */
 
         bot.commands.diskovrCommand = {
-            command: 'diskovr',  //The command to be called. With the standard command literal this would be: !bacon
+            command: 'JAC',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (vif so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("Visit @Diskovr's Website http://Diskovr.net");
+                    API.sendChat("Visit the Offical JAC Website http://jacgaming.com");
                 }
             }
         };
