5168829ab1a39b683dcd8a5a585ce47595657516
Darkwing1012 <dylan8522@gmail.com>
2015-06-19 15:21:59 -0400

    Update JACBot.js

--- JACBot.js
+++ JACBot.js
@@ -2517,6 +2517,19 @@
                     }
                 }
             },
+            
+                JACCommand: {
+                command: 'jac',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat('booty');
+                    }
+                }
+            },
 
             statusCommand: {
                 command: 'status',
